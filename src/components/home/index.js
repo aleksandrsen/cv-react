c70a442cbdda8dbd933f195bef5d3b43055e2af0
Aleksandr Oksen <aledor777@gmail.com>
2021-01-22 20:43:15 +0200

    add responsive layout for medium and small screens

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,7 +3,10 @@ import "./index.scss";
 
 const Home = ({ id, name }) => (
     <section className="home sectionItem" id={id}>
-        <h1 className="sectionItemHeader">Hello, I'm {name}</h1>
+        <h1 className="sectionItemHeader">
+            Hello, I'm
+            <span>{name}</span>
+        </h1>
         <div className="position">Frontend developer</div>
     </section>
 );
