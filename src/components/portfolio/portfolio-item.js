3617297e7d92309d522a55c5c3d664d6101d3ae3
Aleksandr Oksen <aledor777@gmail.com>
2021-02-05 20:19:49 +0200

    add hover animation for portfolio item cards

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,15 +1,30 @@
 import React, { useState } from "react";
+import "./index.scss";
 import Lightbox from "react-image-lightbox";
 import "react-image-lightbox/style.css";
+import { useSpring, animated } from "react-spring";
+
+const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.05];
+const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
 const PortfolioItem = ({ project: { name, link, git, screenshots: images } }) => {
     const screenshots = images.map((item) => `http://localhost:3000${item}`);
     const [isOpen, setIsOpen] = useState(false);
     const [activeIdx, setActiveIdx] = useState(0);
+    const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 1, tension: 150, friction: 70 } }));
+
+    const toggleIsOpen = (e) => setIsOpen(!isOpen);
 
     return (
         <li key={name} className="portfolioItem">
-            <img src={screenshots[activeIdx]} alt={name} onClick={(e) => setIsOpen(true)} />
+            <animated.div
+                className="portfolioItem__previewCard"
+                onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+                onMouseLeave={() => set({ xys: [0, 0, 1] })}
+                style={{ transform: props.xys.interpolate(trans) }}
+            >
+                <img src={screenshots[activeIdx]} alt={name} onClick={toggleIsOpen} />
+            </animated.div>
             <span>{name}</span>
             <div className="links">
                 <a href={link} target="blank">
@@ -29,8 +44,8 @@ const PortfolioItem = ({ project: { name, link, git, screenshots: images } }) =>
             {isOpen && (
                 <Lightbox
                     enableZoom={false}
+                    onCloseRequest={toggleIsOpen}
                     mainSrc={screenshots[activeIdx]}
-                    onCloseRequest={() => setIsOpen(false)}
                     nextSrc={screenshots[(activeIdx + 1) % screenshots.length]}
                     prevSrc={screenshots[(activeIdx + screenshots.length - 1) % screenshots.length]}
                     onMoveNextRequest={() => setActiveIdx((activeIdx + 1) % screenshots.length)}
