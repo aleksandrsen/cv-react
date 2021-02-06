4216af898edc6db1af044bf5c71363b2cfbbe0c2
Aleksandr Oksen <aledor777@gmail.com>
2021-02-06 17:40:44 +0200

    add simple animation for svg icons

--- src/components/experience/index.js
+++ src/components/experience/index.js
@@ -5,12 +5,23 @@ const Experience = ({ id, experience, componentRef }) => (
     <section className="experience sectionItem" id={id} ref={componentRef}>
         <h2 className="sectionItemHeader">{id}</h2>
         <ul className="experience__list">
-            {experience.map(({ orgName, position, period, link, description, iconId }) => (
+            {experience.map(({ orgName, position, period, link, description, iconId }, idx) => (
                 <li className="experience__listItem" key={orgName}>
                     <div>
-                        <svg className="experience__itemIcon" width="100" height="100">
-                            <use xlinkHref={iconId} />
-                        </svg>
+                        {!idx ? (
+                            <svg className="experience__itemIcon" width="100" height="100" viewBox="0 0 640 512">
+                                {/*<use xlinkHref={iconId} />*/}
+                                <path
+                                    d="M255.03 261.65c6.25 6.25 16.38 6.25 22.63 0l11.31-11.31c6.25-6.25 6.25-16.38 0-22.63L253.25 192l35.71-35.72c6.25-6.25 6.25-16.38 0-22.63l-11.31-11.31c-6.25-6.25-16.38-6.25-22.63 0l-58.34 58.34c-6.25 6.25-6.25 16.38 0 22.63l58.35 58.34zm96.01-11.3l11.31 11.31c6.25 6.25 16.38 6.25 22.63 0l58.34-58.34c6.25-6.25 6.25-16.38 0-22.63l-58.34-58.34c-6.25-6.25-16.38-6.25-22.63 0l-11.31 11.31c-6.25 6.25-6.25 16.38 0 22.63L386.75 192l-35.71 35.72c-6.25 6.25-6.25 16.38 0 22.63zM624 416H381.54c-.74 19.81-14.71 32-32.74 32H288c-18.69 0-33.02-17.47-32.77-32H16c-8.8 0-16 7.2-16 16v16c0 35.2 28.8 64 64 64h512c35.2 0 64-28.8 64-64v-16c0-8.8-7.2-16-16-16zM576 48c0-26.4-21.6-48-48-48H112C85.6 0 64 21.6 64 48v336h512V48zm-64 272H128V64h384v256z"
+                                />
+                            </svg>
+                        ) : (
+                            <svg className="experience__itemIcon" width="100" height="100" viewBox="0 0 640 512">
+                                <path
+                                    d="M501.1 395.7L384 278.6c-23.1-23.1-57.6-27.6-85.4-13.9L192 158.1V96L64 0 0 64l96 128h62.1l106.6 106.6c-13.6 27.8-9.2 62.3 13.9 85.4l117.1 117.1c14.6 14.6 38.2 14.6 52.7 0l52.7-52.7c14.5-14.6 14.5-38.2 0-52.7zM331.7 225c28.3 0 54.9 11 74.9 31l19.4 19.4c15.8-6.9 30.8-16.5 43.8-29.5 37.1-37.1 49.7-89.3 37.9-136.7-2.2-9-13.5-12.1-20.1-5.5l-74.4 74.4-67.9-11.3L334 98.9l74.4-74.4c6.6-6.6 3.4-17.9-5.7-20.2-47.4-11.7-99.6.9-136.6 37.9-28.5 28.5-41.9 66.1-41.2 103.6l82.1 82.1c8.1-1.9 16.5-2.9 24.7-2.9zm-103.9 82l-56.7-56.7L18.7 402.8c-25 25-25 65.5 0 90.5s65.5 25 90.5 0l123.6-123.6c-7.6-19.9-9.9-41.6-5-62.7zM64 472c-13.2 0-24-10.8-24-24 0-13.3 10.7-24 24-24s24 10.7 24 24c0 13.2-10.7 24-24 24z"
+                                />
+                            </svg>
+                        )}
                         <div className="experience__listItemInfo">
                             <div className="orgName">
                                 <a href={link} target="blank">
